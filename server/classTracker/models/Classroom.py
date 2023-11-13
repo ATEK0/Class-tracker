33fe81a738272ef76bd4c83c4ed010eb212f87f0
Kayun <kayunowna@gmail.com>
2023-11-13 11:37:20 +0000

    Modify Classroom model

--- server/classTracker/models/Classroom.py
+++ server/classTracker/models/Classroom.py
@@ -4,8 +4,9 @@ class Classroom(db.Model):
     __tablename__ = 'classrooms'
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     tcs_id = db.Column(db.Integer, db.ForeignKey('teachers_cs.id'))
-    begin = db.Column(db.Date)
-    end = db.Column(db.Date)
+    day = db.Column(db.Date)
+    begin = db.Column(db.Time)
+    end = db.Column(db.Time)
     state = db.Column(db.String(255))
 
     teacher_cs = db.relationship('Teacher_CS', back_populates='classroom')
