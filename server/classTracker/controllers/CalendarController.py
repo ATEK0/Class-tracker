04fef9d8a852056c591a17ac93f709d40fa5f341
Im-Kayun <kayunowna@gmail.com>
2023-11-15 11:37:51 +0000

    Classes showing on Calendar

--- server/classTracker/controllers/CalendarController.py
+++ server/classTracker/controllers/CalendarController.py
@@ -4,6 +4,7 @@ from .. import db
 
 from ..models.Teacher_CS import Teacher_CS
 from ..models.Classroom import Classroom
+from ..models.Class_Subject import Class_Subject
 
 
 
@@ -19,7 +20,6 @@ def getCalendarEvents():
 
         teacher_cs_records = Teacher_CS.query.filter_by(teacher=selected_id).all()
 
-
         classroom_ids = [teacher_cs_record.id for teacher_cs_record in teacher_cs_records]
 
         classrooms = Classroom.query.filter(Classroom.tcs_id.in_(classroom_ids)).all()
@@ -28,13 +28,27 @@ def getCalendarEvents():
     elif "class" in selected_id:
         selected_id = selected_id[6::]
 
+        class_subjects_records = Class_Subject.query.filter_by(class_id = selected_id).all()
+
+        class_subject_ids = [class_subject_record.id for class_subject_record in class_subjects_records]
+
+        teacher_cs_records = Teacher_CS.query.filter(Teacher_CS.csid.in_(class_subject_ids)).all()
+
+        classroom_ids = [teacher_cs_record.id for teacher_cs_record in teacher_cs_records]
+
+        classrooms = Classroom.query.filter(Classroom.tcs_id.in_(classroom_ids)).all()
+
+        print(classrooms)
+
+       
+
     for classroom in classrooms:
             classroom_data.append({
                 'id': classroom.id,
                 'title': f"{classroom.teacher_cs.class_subject.class_.grade}º{classroom.teacher_cs.class_subject.class_.label} - {classroom.teacher_cs.class_subject.subject.label}",
                 'start': f"{classroom.day}T{classroom.begin}",
                 'end': f"{classroom.day}T{classroom.end}",
-                'color': 'red'
+                'color': 'green'
             })
     
     print("################ cs data", classroom_data)
