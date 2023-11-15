38266616e9ac1296aac44d1b5d900801589fafde
Im-Kayun <kayunowna@gmail.com>
2023-11-15 16:56:55 +0000

    Add Colors to calendar

--- server/classTracker/controllers/CalendarController.py
+++ server/classTracker/controllers/CalendarController.py
@@ -5,6 +5,9 @@ from .. import db
 from ..models.Teacher_CS import Teacher_CS
 from ..models.Classroom import Classroom
 from ..models.Class_Subject import Class_Subject
+from ..models.User import User
+
+import datetime
 
 
 
@@ -15,6 +18,10 @@ def getCalendarEvents():
     selected_id = request.json["id"]
     classroom_data = []
 
+    currentDate = datetime.datetime.now()
+
+    #print(currentDate)
+
     if "teacher" in selected_id:
         selected_id = selected_id[8::]
 
@@ -24,11 +31,31 @@ def getCalendarEvents():
 
         classrooms = Classroom.query.filter(Classroom.tcs_id.in_(classroom_ids)).all()
 
+        for classroom in classrooms:
+            date = classroom.day
+            startTime = classroom.begin
+            endTime = classroom.end
+            classroomStartDate = datetime.datetime.combine(date, startTime)
+            classroomEndDate = datetime.datetime.combine(date, endTime)
+            if currentDate > classroomEndDate:
+                classroom_color = "#FF3213"
+            elif currentDate >= classroomStartDate and currentDate <= classroomEndDate:
+                classroom_color = "#A171A"
+            else:
+                classroom_color = "#19F705"
+            
+            classroom_data.append({
+                'id': classroom.id,
+                'title': f"{classroom.teacher_cs.class_subject.class_.grade}º{classroom.teacher_cs.class_subject.class_.label} - {classroom.teacher_cs.class_subject.subject.label}",
+                'start': f"{classroom.day}T{classroom.begin}",
+                'end': f"{classroom.day}T{classroom.end}",
+                'color': classroom_color
+            })
 
     elif "class" in selected_id:
         selected_id = selected_id[6::]
 
-        class_subjects_records = Class_Subject.query.filter_by(class_id = selected_id).all()
+        class_subjects_records = Class_Subject.query.filter_by(class_id=selected_id).all()
 
         class_subject_ids = [class_subject_record.id for class_subject_record in class_subjects_records]
 
@@ -38,19 +65,17 @@ def getCalendarEvents():
 
         classrooms = Classroom.query.filter(Classroom.tcs_id.in_(classroom_ids)).all()
 
-        print(classrooms)
-
-       
-
-    for classroom in classrooms:
+        for classroom in classrooms:
+            teacher = User.query.filter_by(id=classroom.teacher_cs.teacher).first()
             classroom_data.append({
                 'id': classroom.id,
-                'title': f"{classroom.teacher_cs.class_subject.class_.grade}º{classroom.teacher_cs.class_subject.class_.label} - {classroom.teacher_cs.class_subject.subject.label}",
+                'title': f"{classroom.teacher_cs.class_subject.subject.label} - {teacher.name} {teacher.surname}",
                 'start': f"{classroom.day}T{classroom.begin}",
                 'end': f"{classroom.day}T{classroom.end}",
-                'color': 'green'
+                'color': '#507a96'
             })
-    
+
     print("################ cs data", classroom_data)
 
     return jsonify(classroom_data)
+
