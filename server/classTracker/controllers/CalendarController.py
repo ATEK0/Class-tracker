101924f92b669a46147706f8cf3bdbdb958d6803
Kayun <kayunowna@gmail.com>
2023-11-28 16:41:36 +0000

    Rework Calendar Queries

--- server/classTracker/controllers/CalendarController.py
+++ server/classTracker/controllers/CalendarController.py
@@ -6,6 +6,7 @@ from ..models.Teacher_CS import Teacher_CS
 from ..models.Classroom import Classroom
 from ..models.Class_Subject import Class_Subject
 from ..models.User import User
+from ..models.Teacher import Teacher
 
 import datetime
 
@@ -25,21 +26,22 @@ def getCalendarEvents():
     if "teacher" in selected_id:
         selected_id = selected_id[8:]
 
-        # teacher_cs_records = Teacher_CS.query.filter_by(teacher=selected_id).all()
-        # classroom_ids = [record.id for record in teacher_cs_records]
-        # classrooms = Classroom.query.filter(Classroom.tcs_id.in_(classroom_ids)).all()
+        teacher = Teacher.query.filter_by(user_id = selected_id).first()
+        teacher_cs_records = Teacher_CS.query.filter_by(teacher_id=teacher.teacher_id).all()
+        classroom_ids = [record.id for record in teacher_cs_records]
+        classrooms = Classroom.query.filter(Classroom.tcs_id.in_(classroom_ids)).all()
 
-        classrooms = (
-            db.session.query(Classroom)
-            .join(Teacher_CS, Classroom.tcs_id == Teacher_CS.id)
-            .filter(Teacher_CS.teacher == selected_id)
-            .all()
-        )
+        # classrooms = (
+        #     db.session.query(Classroom)
+        #     .join(Teacher_CS, Classroom.tcs_id == Teacher_CS.id)
+        #     .filter(Teacher_CS.teacher_id == selected_id)
+        #     .all()
+        # )
 
         for classroom in classrooms:
             start_time = datetime.datetime.combine(classroom.day, classroom.begin)
             end_time = datetime.datetime.combine(classroom.day, classroom.end)
-            classroom_color = classroomState.beforeColor if currentDate > end_time else classroomState.currentColor if currentDate >= start_time else classroomState.afterColor
+            classroom_color = classroomState[0]['afterColor'] if currentDate > end_time else classroomState[0]['currentColor'] if currentDate >= start_time else classroomState[0]['beforeColor']
             
             classroom_data.append({
                 'id': classroom.id,
