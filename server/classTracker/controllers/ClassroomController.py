04c8459321a2e17fe21d7aae90405f6884b24c28
Kayun <kayunowna@gmail.com>
2023-11-28 16:42:38 +0000

    Rework Classroom Queries

--- server/classTracker/controllers/ClassroomController.py
+++ server/classTracker/controllers/ClassroomController.py
@@ -5,13 +5,15 @@ from .. import db
 from ..models.Classroom import Classroom
 from ..models.Class_Subject import Class_Subject
 from ..models.Teacher_CS import Teacher_CS
+from ..models.User import isAdmin
+from ..models.Teacher import Teacher, isTeacher
 
 classroomController = Blueprint('classroomController', __name__)
 
 @classroomController.route("/createClassroom", methods=["POST"])
 def createClassroom():
 
-    teacher_id = request.json["teacher"]
+    user_id = request.json["teacher"]
     subject_id = request.json["subject"]
     class_ID = request.json["class_ID"]
     date = request.json["date"]
@@ -19,7 +21,8 @@ def createClassroom():
     endTime = request.json["endTime"]
 
     classSubject = Class_Subject.query.filter_by(subject_id = subject_id, class_id = class_ID).first()
-    teacherCS = Teacher_CS.query.filter_by(csid = classSubject.id, teacher = teacher_id).first()
+    teacher = Teacher.query.filter_by(user_id = user_id).first()
+    teacherCS = Teacher_CS.query.filter_by(csid = classSubject.id, teacher_id = teacher.teacher_id).first()
     
     newClassroom = Classroom(tcs_id = teacherCS.id, day = date, begin = beginTime, end = endTime, state = "Teste")
 
@@ -33,13 +36,11 @@ def createClassroom():
 
 @classroomController.route("/getClassroomsCount", methods=["GET"])
 def getClassroomsCount():
-
-    userType = request.args.get("type")
     user_id = session.get("user_id")
 
-    if userType == "Admin":
+    if isAdmin(user_id):
         count = Classroom.query.count()
-    elif userType == "Teacher":
+    elif isTeacher(user_id):
         ... #retorna a contagem de turmas que o professor leciona
 
     #fazer igual para os outros endpoints do get...Count
