bb694e9fcc9e5ced3bdcd44372a5588f8c3782a9
Kayun <kayunowna@gmail.com>
2023-12-09 16:37:27 +0000

    Profile Image Endpoints

--- server/classTracker/controllers/ProfileController.py
+++ server/classTracker/controllers/ProfileController.py
@@ -4,6 +4,11 @@ from .. import db, bcrypt
 
 from ..models.User import User
 
+import io
+import os
+from PIL import Image
+import base64
+
 profileController = Blueprint('profileController', __name__)
 
 @profileController.route("/changeProfilePassword", methods=["POST"])
@@ -30,9 +35,37 @@ def changeProfilePassword():
 def updateProfileImage():
 
     user_id = session.get("user_id")
+    user = User.query.filter_by(id = user_id).first()
 
     image_data = request.files.get("image")
 
-    print(image_data)
+    print(image_data.filename)
+
+    filename = f"{user_id}.png"
+
+    try:
+        with open(f"profile_images/{filename}", "wb") as f:
+            f.write(image_data.read())
+    except FileNotFoundError:
+        os.makedirs("profile_images")
+        with open(f"profile_images/{filename}", "wb") as f:
+            f.write(image_data.read())
+
+    user.image_path = f"profile_images/{filename}"
+    db.session.commit()
 
     return ({"a": "a"})
+
+@profileController.route("/getProfileImage", methods=["GET"])
+def getProfileImage():
+
+    user_id = session.get("user_id")
+    user = User.query.filter_by(id = user_id).first()
+    image_path = user.image_path
+
+    if image_path:
+        with open(image_path, 'rb') as f:
+            image_data = f.read()
+            return image_data
+    else:
+        return jsonify({"error": "No profile image found"}), 404
