27b214fa388122e8b334a499981bc07bc4d2b055
Kayun <kayunowna@gmail.com>
2023-12-22 11:07:19 +0000

    Remove ON DELETE CASCADE from parents

--- server/classTracker/models/Student.py
+++ server/classTracker/models/Student.py
@@ -10,7 +10,7 @@ class Student(User, db.Model):
     __tablename__ = 'students'
     student_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     user_id = db.Column(db.String(32), db.ForeignKey('users.id', ondelete='CASCADE'), unique=True)
-    parent_id = db.Column(db.Integer, db.ForeignKey('parents.id', ondelete='CASCADE'))
+    parent_id = db.Column(db.Integer, db.ForeignKey('parents.id'))
     process_number = db.Column(db.String(32))
     class_id = db.Column(db.Integer, db.ForeignKey('classes.id', ondelete='CASCADE'))
 
