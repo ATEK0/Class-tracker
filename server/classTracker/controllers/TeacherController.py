4e93ad249cb72ecd8292abb99333ce43d6aca994
Kayun <kayunowna@gmail.com>
2024-01-03 16:47:22 +0000

    Add getTeacherInfo endpoint

--- server/classTracker/controllers/TeacherController.py
+++ server/classTracker/controllers/TeacherController.py
@@ -5,6 +5,9 @@ from .. import db
 from ..models.User import User, isAdmin
 from ..models.Teacher import Teacher
 from ..models.Class_Subject import Class_Subject
+from ..models.Teacher_CS import Teacher_CS
+from ..models.Class_ import Class_
+from ..models.Subject import Subject
 
 teacherController = Blueprint('teacher', __name__)
 
@@ -39,3 +42,48 @@ def getTeachers():
         } for teacher in teachers] 
 
     return jsonify(teachers_info)
+
+@teacherController.route("/getTeacherInfo", methods=["POST"])
+def getTeacherInfo():
+    current_user = session.get("user_id")
+
+    if not current_user:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    user_id = request.json["id"]
+
+    teacher = Teacher.query.filter_by(user_id = user_id).first()
+    teacher_cs_records = Teacher_CS.query.filter_by(teacher_id = teacher.teacher_id).all()
+    csids = [record.csid for record in teacher_cs_records]
+
+    class_ids_records = Class_Subject.query.filter(Class_Subject.id.in_(csids)).all()
+    class_ids = [record.class_id for record in class_ids_records]
+    class_ids = set(class_ids)
+
+    classes = Class_.query.filter(Class_.id.in_(class_ids)).all()
+
+    classes_list = [str(record.grade) + "º " + record.label for record in classes]
+
+    subject_ids_records = Class_Subject.query.filter(Class_Subject.id.in_(csids)).all()
+    subject_ids = [record.subject_id for record in subject_ids_records]
+    subject_ids = set(subject_ids)
+
+    subjects = Subject.query.filter(Subject.id.in_(subject_ids)).all()
+
+    subjects_list = [record.label for record in subjects]
+
+    teacher_info = {
+        "id": teacher.id,
+        "name": teacher.name,
+        "surname": teacher.surname,
+        "email": teacher.email,
+        "state": teacher.state,
+        "type": "Teacher",
+        "address": teacher.address,
+        "birthdate": teacher.birthdate,
+        "contact": teacher.contact,
+        "subjects": subjects_list,
+        "classes": classes_list
+    }
+
+    return jsonify(teacher_info)
