7c2ebd260cb10c4e076b4f17d59a94554cad01b0
Kayun <kayunowna@gmail.com>
2024-01-25 17:18:44 +0000

    Add is_deleted field to teachers

--- server/classTracker/models/Teacher.py
+++ server/classTracker/models/Teacher.py
@@ -11,5 +11,7 @@ class Teacher(User, db.Model):
     teacher_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     user_id = db.Column(db.String(32), db.ForeignKey('users.id', ondelete='CASCADE'))
     contact = db.Column(db.String(32))
+    is_deleted = db.Column(db.Boolean, default=False)
+
 
     user = db.relationship("User")
