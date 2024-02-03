c15f67b0184bd2c86e4acbdf318ccd93f8fc83a7
Im-Kayun <kayunowna@gmail.com>
2024-02-03 15:25:25 +0000

    Remove title from summaries

--- server/classTracker/models/Summary.py
+++ server/classTracker/models/Summary.py
@@ -4,7 +4,6 @@ from sqlalchemy.dialects.mysql import LONGTEXT
 class Summary(db.Model):
     __tablename__ = 'summaries'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(50))
     content = db.Column(LONGTEXT)
     classroom_id = db.Column(db.Integer, db.ForeignKey('classrooms.id', ondelete='CASCADE'), unique=True)
 
