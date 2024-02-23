f39b263edbabcfcb5baf30f4baf07c06628bab09
Im-Kayun <kayunowna@gmail.com>
2024-02-23 10:43:07 +0000

    Add unique phone number

--- server/classTracker/models/Teacher.py
+++ server/classTracker/models/Teacher.py
@@ -14,7 +14,7 @@ class Teacher(User, db.Model):
     __tablename__ = 'teachers'
     teacher_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     user_id = db.Column(db.String(32), db.ForeignKey('users.id', ondelete='CASCADE'))
-    contact = db.Column(db.String(32))
+    contact = db.Column(db.String(32), unique=True)
     is_deleted = db.Column(db.Boolean, default=False)
 
     
